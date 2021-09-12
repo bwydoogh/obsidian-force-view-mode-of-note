83aff25e6489ea5ce3c7994b24e5cf016e287f5d
Benny Wydooghe <benny.wydooghe@gmail.com>
2021-09-12 10:13:10 +0200

    #4 - Add setting which prevents switching to view mode when note is open

--- main.ts
+++ main.ts
@@ -1,13 +1,41 @@
-import { WorkspaceLeaf, Plugin, MarkdownView } from 'obsidian';
+import { WorkspaceLeaf, Plugin, MarkdownView, App, TFile, PluginSettingTab, Setting, EventRef, debounce } from 'obsidian';
+
+interface ViewModeByFrontmatterSettings {
+	ignoreOpenFiles: boolean;
+}
+
+const DEFAULT_SETTINGS: ViewModeByFrontmatterSettings = {
+	ignoreOpenFiles: false
+}
 
 export default class ViewModeByFrontmatterPlugin extends Plugin {
+	settings: ViewModeByFrontmatterSettings;
+
 	OBSIDIAN_UI_MODE_KEY = 'obsidianUIMode';
 
+	openedFiles: String[];
+
 	async onload() {
+		await this.loadSettings();
+
+		this.addSettingTab(new ViewModeByFrontmatterSettingTab(this.app, this));
+
+		this.openedFiles = resetOpenedNotes(this.app);
+
 		const readViewModeFromFrontmatterAndToggle = async (leaf: WorkspaceLeaf) => {
 			let view = leaf.view instanceof MarkdownView ? leaf.view : null;
 
 			if (null === view) {
+				this.openedFiles = resetOpenedNotes(this.app);
+
+				return;
+			}
+
+			// if setting is true, nothing to do if this was an open note
+			if (true == this.settings.ignoreOpenFiles
+				&& alreadyOpen(view.file, this.openedFiles)) {
+				this.openedFiles = resetOpenedNotes(this.app);
+
 				return;
 			}
 
@@ -26,6 +54,8 @@ export default class ViewModeByFrontmatterPlugin extends Plugin {
 					leaf.setViewState(state);
 				}
 
+				this.openedFiles = resetOpenedNotes(this.app);
+
 				return;
 			}
 
@@ -38,10 +68,88 @@ export default class ViewModeByFrontmatterPlugin extends Plugin {
 
 				leaf.setViewState(state);
 			}
+
+			this.openedFiles = resetOpenedNotes(this.app);
+
+			return;
 		};
 
 		// "active-leaf-change": open note, navigate to note -> will check whether
 		// the view mode needs to be set; default view mode setting is ignored.
-		this.registerEvent(this.app.workspace.on("active-leaf-change", readViewModeFromFrontmatterAndToggle));
+		this.registerEvent(this.app.workspace.on("active-leaf-change", debounce(readViewModeFromFrontmatterAndToggle, 100)));
+	}
+
+	async loadSettings() {
+		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	}
+
+	async saveSettings() {
+		await this.saveData(this.settings);
+	}
+
+	async onunload() {
+		this.openedFiles = [];
 	}
 }
+
+function alreadyOpen(
+	currFile: TFile,
+	openedFiles: String[]
+): boolean {
+	const leavesWithSameNote: String[] = [];
+
+	if (currFile == null) {
+		return false;
+	}
+
+	openedFiles.forEach((openedFile: String) => {
+		if (openedFile == currFile.basename) {
+			leavesWithSameNote.push(openedFile);
+		}
+	});
+
+	return leavesWithSameNote.length != 0;
+}
+
+function resetOpenedNotes(app: App): String[]{
+	let openedFiles : String[] = [];
+
+	app.workspace.iterateAllLeaves((leaf) => {
+		let view = leaf.view instanceof MarkdownView ? leaf.view : null;
+
+		if (null === view) {
+			return;
+		}
+
+		openedFiles.push(leaf.view?.file?.basename);
+	});
+
+	return openedFiles;
+}
+
+class ViewModeByFrontmatterSettingTab extends PluginSettingTab {
+	plugin: ViewModeByFrontmatterPlugin;
+
+	constructor(app: App, plugin: ViewModeByFrontmatterPlugin) {
+		super(app, plugin);
+		this.plugin = plugin;
+	}
+
+	display(): void {
+		let { containerEl } = this;
+
+		containerEl.empty();
+
+		new Setting(containerEl)
+			.setName('Ignore opened files')
+			.setDesc('Never change the view mode on a note which was already open.')
+			.addToggle(checkbox => checkbox
+				.setValue(this.plugin.settings.ignoreOpenFiles)
+				.onChange(async (value) => {
+					this.plugin.settings.ignoreOpenFiles = value;
+					await this.plugin.saveSettings();
+				}))
+			;
+	}
+}
+
